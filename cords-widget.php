a537bdbd3a9ce2b2c703399f6f3dbc4ee71d2d2d
Billy Hawkes <billyhawkes02@gmail.com>
2023-05-02 19:00:26 -0400

    Added meta option to pages and posts, and a request on update

--- cords-widget.php
+++ cords-widget.php
@@ -17,9 +17,79 @@ class CORDSWidgetPlugin
 		// Settings Setup
 		add_action("admin_init", array($this, "settings"));
 
+		// Post Meta Box
+		add_action("add_meta_boxes", array($this, "add_post_meta_boxes"));
+		/* Save post meta on the 'save_post' hook. */
+		add_action('save_post', array($this, "cords_save_post_meta"), 10, 2);
+
+
 		// Add Widget
 		add_action("wp_footer", array($this, "widget"));
 	}
+
+	function cords_save_post_meta($post_id, $post)
+	{
+		/* Verify the nonce before proceeding. */
+		if (!isset($_POST['cords_name_nonce']) || !wp_verify_nonce($_POST['cords_name_nonce'], basename(__FILE__))) {
+			return $post_id;
+		}
+
+		/* Get the post type object. */
+		$post_type = get_post_type_object($post->post_type);
+		/* Check if the current user has permission to edit the post. */
+		if (!current_user_can($post_type->cap->edit_post, $post_id)) {
+			return $post_id;
+		}
+
+		/* Get the posted data and sanitize it for use as an HTML class. */
+		$new_meta_value = (isset($_POST['cords_name']) ? sanitize_text_field($_POST['cords_name']) : '');
+
+		/* Get the meta key. */
+		$meta_key = 'cords_name';
+
+		/* Get the meta value of the custom field key. */
+		$meta_value = get_post_meta($post_id, $meta_key, true);
+
+		$changed = true;
+		/* If a new meta value was added and there was no previous value, add it. */
+		if ($new_meta_value && '' == $meta_value) {
+			add_post_meta($post_id, $meta_key, $new_meta_value, true);
+		}
+		/* If the new meta value does not match the old value, update it. */ elseif ($new_meta_value && $new_meta_value != $meta_value) {
+			update_post_meta($post_id, $meta_key, $new_meta_value);
+		}
+		/* If there is no new meta value but an old value exists, delete it. */ elseif ('' == $new_meta_value && $meta_value) {
+			delete_post_meta($post_id, $meta_key, $meta_value);
+		} else {
+			$changed = false;
+		}
+		if ($changed) {
+			wp_remote_post(
+				"http://localhost:3000/api/cords",
+				array(
+					'method' => 'POST',
+					'body' => json_encode(array($meta_key => $new_meta_value)),
+				)
+			);
+		}
+	}
+
+	function add_post_meta_boxes()
+	{
+		add_meta_box("cords_post_meta_data", "CORDS", array($this, "post_meta_box_html"), ["page", "post"], "normal", "high");
+	}
+
+	function post_meta_box_html($post)
+	{ ?>
+		<?php wp_nonce_field(basename(__FILE__), 'cords_name_nonce'); ?>
+		<div>
+			<label for="cords_name">
+				Name
+			</label>
+			<input type="text" name="cords_name" value="<?php echo esc_attr(get_post_meta($post->ID, 'cords_name', true)) ?>">
+		</div>
+	<?php }
+
 
 	// Settings page setup 
 	function settings()
@@ -73,7 +143,7 @@ class CORDSWidgetPlugin
 	// Settings page html form
 	function settingsHTML()
 	{
-		require_once plugin_dir_path(__FILE__) . "page.php";
+		require_once plugin_dir_path(__FILE__) . "admin.php";
 	}
 
 	// Adds widget html 
