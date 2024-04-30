243738c03cf06fc17c64f8dcc94fb7dc8edeaec4
Billy Hawkes <billyhawkes02@gmail.com>
2024-04-30 11:15:46 -0400

    Added nearest neighbour to sdk and resource page

--- apps/widget/src/routes/Resource.tsx
+++ apps/widget/src/routes/Resource.tsx
@@ -1,10 +1,65 @@
-import { formatServiceAddress } from "@cords/sdk";
-import { useNavigate, useParams } from "@solidjs/router";
+import { ResourceType, formatServiceAddress } from "@cords/sdk";
+import { A, useNavigate, useParams, useSearchParams } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
-import { Show, Suspense } from "solid-js";
-import Related from "../components/Related";
+import { convert } from "html-to-text";
+import { Component, For, Show, Suspense } from "solid-js";
 import { clipboardIDs, setClipboardIDs } from "../lib/clipboard";
 import { useCords } from "../lib/cords";
+import { location } from "../lib/location";
+
+const RelatedItem: Component<{
+	service: ResourceType;
+}> = (props) => {
+	const [query] = useSearchParams();
+	return (
+		<A
+			href={`/resource/${props.service.id}?${new URLSearchParams(query).toString()}`}
+			class="bg-primary hover:bg-opacity-10 bg-opacity-5 rounded-lg border border-primary p-3 flex flex-col gap-1.5 items-start"
+		>
+			<p class="font-header text-sm text-primary">{props.service.name.en}</p>
+			<p class="text-xs line-clamp-2 max-w-full">{convert(props.service.description.en)}</p>
+		</A>
+	);
+};
+
+const Related: Component<{
+	id: string;
+}> = (props) => {
+	const cords = useCords();
+	const related = createQuery(() => ({
+		queryKey: ["related", props.id],
+		queryFn: () => cords.related(props.id),
+		throwOnError: true,
+		suspense: true,
+	}));
+
+	return (
+		<Show when={related.data?.data.length > 0}>
+			<h3>People have also looked at</h3>
+			<For each={related.data.data}>{(service) => <RelatedItem service={service} />}</For>
+		</Show>
+	);
+};
+
+const Nearest: Component<{
+	id: string;
+}> = (props) => {
+	const cords = useCords();
+	const related = createQuery(() => ({
+		queryKey: ["nearest-neighbour", props.id],
+		queryFn: () =>
+			cords.nearestNeighbour(props.id, { lat: location().lat, lng: location().lng }),
+		throwOnError: true,
+		suspense: true,
+	}));
+
+	return (
+		<Show when={related.data?.data.length > 0}>
+			<h3>Related Resources</h3>
+			<For each={related.data.data}>{(service) => <RelatedItem service={service} />}</For>
+		</Show>
+	);
+};
 
 const Resource = () => {
 	const cords = useCords();
@@ -117,6 +172,11 @@ const Resource = () => {
 						{resource.data.website.en}
 					</a>
 				</div>
+				<Suspense>
+					<Show when={resource.data.id}>
+						<Nearest id={resource.data.id} />
+					</Show>
+				</Suspense>
 				<Suspense>
 					<Show when={resource.data.id}>
 						<Related id={resource.data.id} />
