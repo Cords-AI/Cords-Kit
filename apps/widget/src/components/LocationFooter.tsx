69bbde5fd888e1903990707a62de584ac5491aca
Billy Hawkes <billyhawkes02@gmail.com>
2024-06-24 12:33:03 -0400

    Added small cords logo bottom right

--- apps/widget/src/components/LocationFooter.tsx
+++ apps/widget/src/components/LocationFooter.tsx
@@ -1,4 +1,5 @@
 import { A } from "@solidjs/router";
+import logo from "../assets/logo.svg";
 import { location } from "../lib/location";
 import { useSearchParams } from "../lib/params";
 import { useTranslation } from "../translations";
@@ -8,13 +9,16 @@ const LocationFooter = () => {
 	const [query] = useSearchParams();
 
 	return (
-		<footer class="bg-elevation1 px-4 text-[10px] flex items-center gap-2 border-t">
-			<div class="bg-orange-300 w-2 h-2 rounded-full" />
-			<p class="font-medium">{location() ? location().name : "Loading location..."}</p>
-			<span class="font-bold">•</span>
-			<A href={`/location?${new URLSearchParams(query).toString()}`} class="text-primary">
-				{t().location.change}
-			</A>
+		<footer class="bg-elevation1 px-4 flex justify-between border-t">
+			<div class="flex items-center gap-2 text-[10px]">
+				<div class="bg-orange-300 w-2 h-2 rounded-full" />
+				<p class="font-medium">{location() ? location().name : "Loading location..."}</p>
+				<span class="font-bold">•</span>
+				<A href={`/location?${new URLSearchParams(query).toString()}`} class="text-primary">
+					{t().location.change}
+				</A>
+			</div>
+			<img src={logo} class="w-10" />
 		</footer>
 	);
 };
