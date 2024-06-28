7b40d81d109c9dab6eb54c5342e24373f3022fb9
Billy Hawkes <billyhawkes02@gmail.com>
2024-06-28 10:46:00 -0400

    Added pp and terms

--- apps/widget/src/components/LocationFooter.tsx
+++ apps/widget/src/components/LocationFooter.tsx
@@ -10,7 +10,7 @@ const LocationFooter = () => {
 
 	return (
 		<footer class="bg-elevation1 px-4 py-3 flex justify-between border-t">
-			<div class="flex flex-col text-[10px]">
+			<div class="flex flex-col text-[10px] justify-between">
 				<div class="flex items-center">
 					<div class="bg-orange-300 w-2 h-2 mr-2 rounded-full" />
 					<span class="font-medium">
@@ -24,14 +24,32 @@ const LocationFooter = () => {
 					{t().location.change}
 				</A>
 			</div>
-			<a
-				href="https://cords.ai"
-				target="_blank"
-				class="text-[10px] flex items-center justify-end gap-1"
-			>
-				{t()["powered-by"]}
-				<img src={logo} class="w-10" />
-			</a>
+			<div class="flex flex-col justify-between">
+				<a
+					href="https://cords.ai"
+					target="_blank"
+					class="text-[10px] flex items-center justify-end gap-1 py-1 -mt-1"
+				>
+					{t()["powered-by"]}
+					<img src={logo} class="w-10" />
+				</a>
+				<div class="flex gap-2">
+					<a
+						href={`https://cords.ai/${query.lang ? query.lang : "en"}/privacy-policy`}
+						target="_blank"
+						class="text-[10px] py-1 -mb-1"
+					>
+						Privacy Policy
+					</a>
+					<a
+						href={`https://cords.ai/${query.lang ? query.lang : "en"}/terms-of-use`}
+						target="_blank"
+						class="text-[10px] py-1 -mb-1"
+					>
+						Terms of Use
+					</a>
+				</div>
+			</div>
 		</footer>
 	);
 };
