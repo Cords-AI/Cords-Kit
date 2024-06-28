a981a55b3bf0d58b3ad34121b40cad8f3a5c596a
Billy Hawkes <billyhawkes02@gmail.com>
2024-06-28 09:49:20 -0400

    Added powered by and link to logo, reformated location footer to fit

--- apps/widget/src/components/LocationFooter.tsx
+++ apps/widget/src/components/LocationFooter.tsx
@@ -9,16 +9,29 @@ const LocationFooter = () => {
 	const [query] = useSearchParams();
 
 	return (
-		<footer class="bg-elevation1 px-4 flex justify-between border-t">
-			<div class="flex items-center gap-2 text-[10px]">
-				<div class="bg-orange-300 w-2 h-2 rounded-full" />
-				<p class="font-medium">{location() ? location().name : "Loading location..."}</p>
-				<span class="font-bold">•</span>
-				<A href={`/location?${new URLSearchParams(query).toString()}`} class="text-primary">
+		<footer class="bg-elevation1 px-4 py-3 flex justify-between border-t">
+			<div class="flex flex-col text-[10px]">
+				<div class="flex items-center">
+					<div class="bg-orange-300 w-2 h-2 mr-2 rounded-full" />
+					<span class="font-medium">
+						{location() ? location().name : "Loading location..."}
+					</span>
+				</div>
+				<A
+					href={`/location?${new URLSearchParams(query).toString()}`}
+					class="text-primary pl-4 py-1 -mb-1"
+				>
 					{t().location.change}
 				</A>
 			</div>
-			<img src={logo} class="w-10" />
+			<a
+				href="https://cords.ai"
+				target="_blank"
+				class="text-[10px] flex items-center justify-end gap-1"
+			>
+				{t()["powered-by"]}
+				<img src={logo} class="w-10" />
+			</a>
 		</footer>
 	);
 };
