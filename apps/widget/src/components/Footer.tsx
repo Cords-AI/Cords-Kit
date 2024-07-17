961009a90c61709300569662db0c2e513d062a30
Billy Hawkes <billyhawkes02@gmail.com>
2024-07-17 14:08:26 -0400

    Fixed location resetting and using old cached value, prevented multiple session requests

--- apps/widget/src/components/Footer.tsx
+++ apps/widget/src/components/Footer.tsx
@@ -14,7 +14,6 @@ const Footer = () => {
 	const mutateSession = useSessionMutation();
 
 	createEffect(() => {
-		console.log(session.data);
 		if (session.data && session.data.address === "Toronto, ON, Canada (Default)") {
 			mutateSession.mutate({
 				...session.data,
