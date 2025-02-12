a9276158aa7da284c872674e2be698bc5ac746b0
Billy Hawkes <billyhawkes02@gmail.com>
2025-02-12 11:45:26 -0500

    refactor: added warning to database url

--- apps/widget/src/server/db.ts
+++ apps/widget/src/server/db.ts
@@ -4,10 +4,15 @@ import * as schema from "./schema";
 
 // Returns database ORM client
 export const getDB = () => {
+	if (!process.env.TURSO_URL || !process.env.TURSO_TOKEN) {
+		throw new Error("TURSO_URL and TURSO_TOKEN must be set");
+	}
+
 	const client = createClient({
-		url: process.env.TURSO_URL!,
-		authToken: process.env.TURSO_TOKEN!,
+		url: process.env.TURSO_URL,
+		authToken: process.env.TURSO_TOKEN,
 	});
+
 	return drizzle(client, {
 		schema,
 	});
