45510356a95a2e01e0b8420bd34c81f747b8a6a0
Billy Hawkes <billyhawkes02@gmail.com>
2025-05-30 14:30:25 -0400

    task: remove wp plugin from cords kit (should be on its own)

--- apps/wp-admin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-element'), 'version' => 'f7e3922bb16dc7f2dd08');
--- apps/wp-admin/src/index.js
+++
@@ -1,5 +0,0 @@
-import { render } from "@wordpress/element";
-import App from "./App";
-
-// Render the App component into the DOM
-render(<App />, document.getElementById("cords"));
